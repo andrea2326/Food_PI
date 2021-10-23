87304f9ad1969a1862602472eb5a9c2b76723a2c
andrea2326 <andrea.solariroa@hotmail.com>
2021-10-22 23:16:37 -0300

    models

--- api/src/db.js
+++ api/src/db.js
@@ -30,10 +30,11 @@ sequelize.models = Object.fromEntries(capsEntries);
 
 // En sequelize.models están todos los modelos importados como propiedades
 // Para relacionarlos hacemos un destructuring
-const { Recipe } = sequelize.models;
-
+const { Recipe, Diet } = sequelize.models;
 // Aca vendrian las relaciones
 // Product.hasMany(Reviews);
+Recipe.belongsToMany(Diet, {through: 'recipes_id'});
+Diet.belongsToMany(Recipe, {through: 'recipes_id'});
 
 module.exports = {
   ...sequelize.models, // para poder importar los modelos así: const { Product, User } = require('./db.js');
--- api/src/models/Recipe.js
+++ api/src/models/Recipe.js
@@ -4,9 +4,37 @@ const { DataTypes } = require('sequelize');
 module.exports = (sequelize) => {
   // defino el modelo
   sequelize.define('recipe', {
+    id:{
+      type: DataTypes.UUID,
+      defaultValue: DataTypes.UUIDV4,
+      primaryKey: true,
+      allowNull: false
+    },
     name: {
       type: DataTypes.STRING,
       allowNull: false,
     },
+    resume: {
+      type: DataTypes.STRING,
+      allowNull: false
+    },
+    score: {
+      type: DataTypes.INTEGER
+    },
+    healthyFoofLevel: {
+      type: DataTypes.INTEGER
+    },
+    img:{
+      type: DataTypes.TEXT
+    },
+    stepByStep: {
+      type: DataTypes.ARRAY(DataTypes.TEXT)
+    },
+    createInDb: {
+      type: DataTypes.STRING,
+      defaultValue: true
+    }
   });
 };
+
+// API Key: 8e4a19927203484c8aeb75b4af288b1e
